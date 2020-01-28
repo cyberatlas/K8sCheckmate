4e35262bbc59a20eed1123a89e693ff6f12b641b
cyberatlas <10457502+cyberatlas@users.noreply.github.com>
2020-01-28 00:48:45 -0600

    Able to get values from dict

--- TestingDir/test.py
+++ TestingDir/test.py
@@ -1,18 +1,24 @@
 import yaml
 import argparse
 
+def get_yaml(values_file) -> values_dict:
+    """get_yaml
 
-def get_yaml(values_file):
+    :param values_file:
+    :rtype: values_dict
+    """
+
     with open(values_file) as f:
         data = yaml.safe_load(f)
         print(f'{data} \n \n')
         print(yaml.dump(data))
 
         print(type(data))
-
-
+        print(data.get("image"))
+        print(type(data.get("image")))
 
 def main():
+
     parser = argparse.ArgumentParser(description = "Testing python implementation for a POC")
     parser.add_argument('valuesYaml', help = "The values.yaml file to pass in")
     args = parser.parse_args()
@@ -20,12 +26,14 @@ def main():
 
     get_yaml(values_file)
 
-"""
+    """
     # TODO implement checking for file types, args, etc
     with open(values_file) as f:
         data = yaml.safe_load(f)
         print(f'{data} \n \n')
         print(yaml.dump(data))
-"""
+    """
 
 main()
+
+
