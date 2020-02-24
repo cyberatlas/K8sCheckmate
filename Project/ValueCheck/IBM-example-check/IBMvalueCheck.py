b926b785dc9afee001594e4d010fb543ed5fc2c6
John Young <younjoh16@gmail.com>
2020-02-24 14:17:33 -0600

    My updates to testing IBM values

---
+++ Project/ValueCheck/IBM-example-check/IBMvalueCheck.py
@@ -0,0 +1,17 @@
+### This file will be checking the values from IBM values.yaml that I found online
+# Will be referencing the yaml file in TestCharts/IBM-example/values.yaml
+
+
+import json
+
+def checkIBMValues(IBM_values, policy_Rules):
+    ### Get policies from policy.yaml
+    ### Put policies in Dictionary
+    policyList = getPolicies(policy_Rules)
+
+    ### Get only the needed values from values.yaml
+    
+
+
+
+    return False
