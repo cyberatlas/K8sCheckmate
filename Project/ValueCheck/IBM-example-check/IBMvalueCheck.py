3ece9ed15c1b96c257c2ae99da6d1266abfcd5f4
John Young <younjoh16@gmail.com>
2020-02-24 18:00:28 -0600

    Starting the process of traversing through the policies file

--- Project/ValueCheck/IBM-example-check/IBMvalueCheck.py
+++ Project/ValueCheck/IBM-example-check/IBMvalueCheck.py
@@ -4,14 +4,27 @@
 
 import json
 
+### Testing the values against the policies
 def checkIBMValues(IBM_values, policy_Rules):
-    ### Get policies from policy.yaml
-    ### Put policies in Dictionary
-    policyList = getPolicies(policy_Rules)
-
-    ### Get only the needed values from values.yaml
-    
+    #### traverse through the policy_Rules
+    for k, v in policy_Rules.items():
+        checkPortNumber(k, v)
 
 
+### Simple check port number
+def checkPortNumber(policy_key, policy_value):
+    ### Test Port number
+    if(policy_value == IBM_values_sample.get(policy_key, 0)):
+        print("Port passes policy")
+    else:
+        print("The values port does not match the intended policy")
 
-    return False
+### Main method
+def main():
+    checkIBMValues(IBM_values_sample, policy_Rules_sample)
+
+
+#### Giving sample input for now
+policy_Rules_sample = {"port" : 80}
+IBM_values_sample = {"port" : 120}
+main()
