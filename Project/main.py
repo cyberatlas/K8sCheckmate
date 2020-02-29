3dd3e8e010bd883bc6adb791b5501880553032fd
John Young <younjoh16@gmail.com>
2020-02-29 17:06:32 -0600

    Updated plan for overall main method in Project Directory

---
+++ Project/main.py
@@ -0,0 +1,19 @@
+import HelmParse.helmparse as HP
+
+def main():
+    # Check File Exists
+        # Throw error
+    
+    # Parse <file> w/ Helm Parser
+
+    # Parse <Policy> w/ Policy Parser
+
+    # Check outputs w/ Value Check
+    
+    # Red/Green terminal While Parsing
+
+    # Log When done (Errors @ top)
+
+    print("Hello World")
+
+main()
