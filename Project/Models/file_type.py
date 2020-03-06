0a520b9dcf0aae92c85ad2ac2ea11382cbcb4d7b
Sean <seansailer28@gmail.com>
2020-03-06 14:32:32 -0600

    Restructuring of the project into an organized view

---
+++ Project/Models/file_type.py
@@ -0,0 +1,8 @@
+from enum import Emum as Enum
+
+export class FileType(Enum):
+    Config = 0,
+    Policy = 1,
+    Chart = 2,
+    JSON = 3,
+    YAML = 4
---
+++ Project/Modules/ErrorHandler/error_handler.py
@@ -0,0 +1,26 @@
+from file_type.py import FileType as FileType
+
+# TODO
+def file_not_found(expectedFileType):
+    if expectedFileType is FileType.Config:
+        this.config_not_found()
+    else if expectedFileType is FileType.Policy:
+        # handle policy file
+    else if expectedFileType is FileType.Chart:
+        # handle chart file
+    else:
+        # handle generic
+
+    exit(-1)
+
+# TODO
+def config_not_found():
+    print('TODO')
+
+# TODO -- more moethods
+
+def main():
+    print('Error Handler');
+
+if __name__ == '__main__':
+    main()
