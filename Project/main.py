b368c0beab0c47a29f9f1ede6f888970a1802cad
John Young <younjoh16@gmail.com>
2020-03-11 12:39:06 -0500

    Added some simple check logic for file's existence

--- Project/main.py
+++ Project/main.py
@@ -3,15 +3,28 @@ from Modules.ValueVerifier import value_verifier as V
 from Modules.OutputHandler import output_handler as O
 from Modules.ErrorHandler import error_handler as E
 from Modules.ConfigHandler import config_handler as C
+from pathlib import Path
+import sys
 
 def main():
+    # Check File Exists
+        # Throw error
+    if(len(sys.argv) < 2):
+        print("Error. Please give a file for the command argument")
+        sys.exit()
+    else:
+        file_path = sys.argv[1]
+        if(Path(file_path).is_file()):
+            print("File exists")
+        else:
+            print("Argument for file is present, but the file path does not exist")
+            sys.exit()
+
     P.main()
     V.main()
     O.main()
     E.main()
     C.main()
-    # Check File Exists
-        # Throw error
     
     # Parse <file> w/ Helm Parser
 
