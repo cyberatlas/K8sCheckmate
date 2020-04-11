397afc88ce8ad2bc7b2d66522837aa4045659b77
Sean <seansailer28@gmail.com>
2020-04-11 14:51:02 -0500

    Addded values dictionary

--- Project/Modules/Parser/parser.py
+++ Project/Modules/Parser/parser.py
@@ -5,23 +5,45 @@ class Parser():
     def __init__(self):
         self.__chart_dict = None
         self.__policy_dict = None
+        self.__values_dict = None
         
 
     def load_chart(self, path):
-        with open(path) as json_file:
-            self.__chart_dict = yaml.load(json_file, yaml.SafeLoader)
+
+        with open(path) as yaml_file:
+            self.__chart_dict = yaml.load(yaml_file, yaml.SafeLoader)
 
     def load_policy(self, path):
-        with open(path) as json_file:
-            self.__policy_dict = yaml.load(json_file, yaml.SafeLoader)
+        with open(path) as yaml_file:
+            self.__policy_dict = yaml.load(yaml_file, yaml.SafeLoader)
 
-    def get_chart_dict(self):
-        return self.__chart_dict
+    def load_values(self, path):
+        with open(path) as yaml_file:
+            self.__values_dict = yaml.load(yaml_file, yaml.SafeLoader)
 
-    def get_policy_dict(self):
-        return self.__policy_dict
+    def get_chart_dict(self, config):
+        if config['chartPath'] != '':
+            self.load_chart(config['chartPath'])
+            return self.__chart_dict
+        else:
+            # handle error
+            print('Err')
 
+    def get_policy_dict(self, config):
+        if config['policyPath'] != '':
+            self.load_policy(config['policyPath'])
+            return self.__policy_dict
+        else:
+            # handle error
+            print('Err')
 
+    def get_values_dict(self, config):
+        if config['valuesPath'] != '':
+            self.load_values(config['valuesPath'])
+            return self.__values_dict
+        else:
+            # handle error
+            print('Err')
 
 # def dictIterate(d, level):
 #     tabs = ''.join('\t' for i in range(0, level))
